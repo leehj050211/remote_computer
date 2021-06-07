7f8bca60b2a5e529a285a2f48c2b3499d4822217
unknown <leehj050211@gmail.com>
2021-06-07 22:03:29 +0900

    마우스휠과 ctrl, F12, 윈도우키 같은 키 입력, delay기능 추가, 안드로이드 앱 개발

--- remote_computer/remote_computer.ino
+++ remote_computer/remote_computer.ino
@@ -1,6 +1,8 @@
 #include <Keyboard.h>
 #include <Mouse.h>
-String command, macro_command;
+String command, macro_command, str;
+char str1;
+int Key;
 int mouse_x=0, mouse_y=0;
 
 void key();
@@ -15,51 +17,96 @@ void loop(){
   if (Serial1.available()){
     command=Serial1.readStringUntil('|');// |까지의 시리얼 값을 받음
     command.trim();// 앞뒤 공백제거
-    if(command.substring(0, 3).equals("Mmo")){
-        int slash_index = command.indexOf('/');// /글자의 위치값 저장
-        int comma_index = command.indexOf(',', slash_index + 1);// ,글자의 위치값 저장
-        if (comma_index > 0){
-          mouse_x = command.substring(slash_index + 1, comma_index).toInt();
-          mouse_y = command.substring(comma_index + 1).toInt();
-        }
-        else{
-          mouse_x = command.substring(slash_index + 1).toInt();
-          mouse_y = 0;
-        }
-        Mouse.move(mouse_x, mouse_y, 0);
-    }else if(command.substring(0, 3).equals("Mlc")){
-        int slash_index = command.indexOf('/');// /글자의 위치값 저장
-        switch(command.substring(slash_index + 1).toInt()){
-          case 0:
-            Mouse.release(MOUSE_LEFT);
-            break;
-          case 1:
-            Mouse.press(MOUSE_LEFT);
-            break;
-          case 2:
-          default:
-            Mouse.press(MOUSE_LEFT);
-            Mouse.release(MOUSE_LEFT);
-            break;
-        }
-    }else if(command.substring(0, 3).equals("Mrc")){
-        int slash_index = command.indexOf('/');// /글자의 위치값 저장
-        switch(command.substring(slash_index + 1).toInt()){
-          case 0:
-            Mouse.release(MOUSE_RIGHT);
-            break;
-          case 1:
-            Mouse.press(MOUSE_RIGHT);
-            break;
-          case 2:
-          default:
-            Mouse.press(MOUSE_RIGHT);
-            Mouse.release(MOUSE_RIGHT);
-            break;
-        }
-    }else if(command.substring(0, 3).equals("Key")){
-        int slash_index = command.indexOf('/');// /글자의 위치값 저장
-        Keyboard.print(command.substring(slash_index + 1));
+    if (command.charAt(0) == '/'){
+      if(command.substring(1, 4).equals("txt")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          Keyboard.print(command.substring(slash_index + 1));
+      }else if(command.substring(1, 4).equals("key")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          int comma_index = command.indexOf(',', slash_index + 1);// ,글자의 위치값 저장
+          Key = command.substring(comma_index + 1).toInt();
+          switch(command.substring(slash_index + 1, comma_index).toInt()){
+            case 0:
+              Keyboard.release(Key);
+              break;
+            case 1:
+              Keyboard.press(Key);
+              break;
+            case 2:
+            default:
+              Keyboard.press(Key);
+              Keyboard.release(Key);
+              break;
+          }
+      }else if(command.substring(1, 4).equals("pre")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          int comma_index = command.indexOf(',', slash_index + 1);// ,글자의 위치값 저장
+          str = command.substring(comma_index + 1);
+          str.toCharArray(str1, str.length()+1);
+          switch(command.substring(slash_index + 1, comma_index).toInt()){
+            case 0:
+              Keyboard.release(str1);
+              break;
+            case 1:
+              Keyboard.press(str1);
+              break;
+            case 2:
+            default:
+              Keyboard.press(str1);
+              Keyboard.release(str1);
+              break;
+          }
+      }else if(command.substring(1, 7).equals("keyall")){
+          Keyboard.releaseAll();
+      }else if(command.substring(1, 4).equals("mm")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          int comma_index = command.indexOf(',', slash_index + 1);// ,글자의 위치값 저장
+          if (comma_index > 0){
+            mouse_x = command.substring(slash_index + 1, comma_index).toInt();
+            mouse_y = command.substring(comma_index + 1).toInt();
+          }
+          else{
+            mouse_x = command.substring(slash_index + 1).toInt();
+            mouse_y = 0;
+          }
+          Mouse.move(mouse_x, mouse_y, 0);
+      }else if(command.substring(1, 3).equals("ml")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          switch(command.substring(slash_index + 1).toInt()){
+            case 0:
+              Mouse.release(MOUSE_LEFT);
+              break;
+            case 1:
+              Mouse.press(MOUSE_LEFT);
+              break;
+            case 2:
+            default:
+              Mouse.press(MOUSE_LEFT);
+              Mouse.release(MOUSE_LEFT);
+              break;
+          }
+      }else if(command.substring(1, 3).equals("mr")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          switch(command.substring(slash_index + 1).toInt()){
+            case 0:
+              Mouse.release(MOUSE_RIGHT);
+              break;
+            case 1:
+              Mouse.press(MOUSE_RIGHT);
+              break;
+            case 2:
+            default:
+              Mouse.press(MOUSE_RIGHT);
+              Mouse.release(MOUSE_RIGHT);
+              break;
+          }
+      }else if(command.substring(1, 3).equals("mw")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          Mouse.move(0, 0, command.substring(slash_index + 1).toInt());
+      }else if(command.substring(1, 6).equals("delay")){
+          int slash_index = command.indexOf('/', 1);// /글자의 위치 값 저장
+          delay(command.substring(slash_index + 1).toInt());
+      }
     }
   }
 }
---
+++ remote_computer/remote_computer/app/src/main/java/com/zzz2757/remote_computer/MainActivity.java
@@ -0,0 +1,197 @@
+package com.zzz2757.remote_computer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "MainActivity";
+    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+
+    TextView textStatus;
+    Button btnParied, btnSearch, btnCommand_Send, btnText_Send;
+    TextView input_command, input_text;
+    ListView listView;
+
+    BluetoothAdapter btAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    ArrayAdapter<String> btArrayAdapter;
+    ArrayList<String> deviceAddressArray;
+
+    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothSocket btSocket = null;
+    ConnectedThread connectedThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get permission
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        ActivityCompat.requestPermissions(MainActivity.this, permission_list, 1);
+
+        // Enable bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        // variables
+        textStatus = (TextView) findViewById(R.id.text_status);
+        btnParied = (Button) findViewById(R.id.btn_paired);
+        btnSearch = (Button) findViewById(R.id.btn_search);
+        btnCommand_Send = (Button) findViewById(R.id.btn_command_send);
+        btnText_Send = (Button) findViewById(R.id.btn_text_send);
+        input_command = (EditText) findViewById(R.id.input_command);
+        input_text = (EditText) findViewById(R.id.input_text);
+        listView = (ListView) findViewById(R.id.listview);
+
+        // Show paired devices
+        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+        listView.setAdapter(btArrayAdapter);
+
+        listView.setOnItemClickListener(new myOnItemClickListener());
+    }
+
+    public void onClickButtonPaired(View view){
+        btArrayAdapter.clear();
+        if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
+        pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+            }
+        }
+    }
+
+    public void onClickButtonSearch(View view){
+        // Check if the device is already discovering
+        if(btAdapter.isDiscovering()){
+            btAdapter.cancelDiscovery();
+        } else {
+            if (btAdapter.isEnabled()) {
+                btAdapter.startDiscovery();
+                btArrayAdapter.clear();
+                if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
+                    deviceAddressArray.clear();
+                }
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(receiver, filter);
+            } else {
+                Toast.makeText(getApplicationContext(), "bluetooth not on", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Send string
+    public void onClickButtonSendCommand(View view){
+        if(connectedThread!=null){connectedThread.write(input_command.getText().toString());}
+    }
+    public void onClickButtonSendText(View view){
+        if(connectedThread!=null){connectedThread.write("/txt/"+input_text.getText().toString()+"|");}
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+                btArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
+    public class myOnItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+
+            textStatus.setText("try...");
+
+            final String name = btArrayAdapter.getItem(position); // get name
+            final String address = deviceAddressArray.get(position); // get address
+            boolean flag = true;
+
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+            // create & connect socket
+            try {
+                btSocket = createBluetoothSocket(device);
+                btSocket.connect();
+            } catch (IOException e) {
+                flag = false;
+                textStatus.setText("connection failed!");
+                e.printStackTrace();
+            }
+
+            // start bluetooth communication
+            if(flag){
+                textStatus.setText("connected to "+name);
+                connectedThread = new ConnectedThread(btSocket);
+                connectedThread.start();
+            }
+
+        }
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+        }
+        return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+    }
+}
